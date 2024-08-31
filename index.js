d3dc20860c3f97235aa4e8cc9f3c98b91359cf7a
Shashwat Tiwari <shivajidubey@Shashwats-MacBook-Air.local>
2024-08-31 22:51:39 +0530

    cors update

--- index.js
+++ index.js
@@ -24,10 +24,7 @@ const __dirname = path.resolve();
 
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:5173', 
-  credentials: true 
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
